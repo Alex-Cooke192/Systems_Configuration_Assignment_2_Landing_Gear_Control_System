5bed019e228f67bc12796891c0cf25bd86f3f4a4
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-09 15:15:30 +0000

    added deploy time calculation to class, no side effects

--- gear_configuration.py
+++ gear_configuration.py
@@ -9,3 +9,18 @@ class GearConfiguration:
     extension_distance_mm: int
     lock_time_ms: int
     requirement_time_ms: int
+
+    def compute_deploy_time_ms(self) -> float:
+        # Worst-case theoretical deploy time.
+        # Pure calculation, no side effects.
+        actuator_speed_mm_per_ms = self.actuator_speed_mm_per_100ms / 100.0
+
+        extension_time_ms = (
+            self.extension_distance_mm / actuator_speed_mm_per_ms
+        )
+
+        return (
+            self.pump_latency_ms +
+            extension_time_ms +
+            self.lock_time_ms
+        )
