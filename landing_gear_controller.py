288df46b1af8211975bfe221e1a3ce6e9b6c78d8
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-09 14:58:40 +0000

    Retrstructured deply function to improve safety & add logs

--- landing_gear_controller.py
+++ landing_gear_controller.py
@@ -1,5 +1,7 @@
 ## Top file comment
 
+import time
+
 class GearState(Enum):
     UP_LOCKED = auto()
     TRANSITIONING_DOWN = auto()
@@ -13,13 +15,19 @@ class LandingGearController:
         print(msg)
 
     def command_gear_down(self):
-        if self.state == GearState.UP_LOCKED:
-            self.state = GearState.TRANSITIONING_DOWN
-            self.log("Gear deploying")
-            self.state = GearState.DOWN_LOCKED
-            self.log("Gear locked down")
-        else:
-            self.log("Command rejected")
+        # Only accept deploy when in UP_LOCKED state 
+        if self.state != GearState.UP_LOCKED:
+            self.log("""Deploy REJECTED\n
+                        Current state: {self.state}\n
+                        Not in UP_LOCKED""")
+            return False
+        
+        # Begin actuation immediately (LGCS-PR001)
+        self.state = GearState.TRANSITIONING_DOWN
+        self.ui_status = "DEPLOYING"
+        self._deploy_start_ts = time.monotonic()
+        self.log("Gear deploying...")
+        
 
 controller = LandingGearController()
 controller.command_gear_down()
