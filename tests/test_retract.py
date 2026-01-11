23f1bee65176fdb6b640a150c0034601b84089b1
Alex Cooke <30250508@blackpool.ac.uk>
2026-01-11 01:42:14 +0000

    corrected boolean args

--- tests/test_retract.py
+++ tests/test_retract.py
@@ -47,7 +47,6 @@ from landing_gear_controller import LandingGearController
 
 
 class FakeClock:
-    # Deterministic clock manually advanced by tests
     def __init__(self, start: float = 0.0):
         self.t: float = float(start)
 
@@ -59,7 +58,6 @@ class FakeClock:
 
 
 class SpyLandingGearController(LandingGearController):
-    # Controller capturing actuator command outputs and log lines
     def __init__(self, config: GearConfiguration, clock):
         super().__init__(config=config, clock=clock)
         self.down_cmds: list[bool] = []
@@ -88,20 +86,26 @@ def config() -> GearConfiguration:
     )
 
 
-# -----------------------------
-# LGCS-FR002 retract command behavior
-# -----------------------------
-
-def test_command_gear_up_happy_path_enters_transition_and_actuates(config):
+def test_command_gear_up_true_from_down_locked_starts_transition(config):
     clock = FakeClock()
     c = SpyLandingGearController(config=config, clock=clock)
 
     c.enter_state(GearState.DOWN_LOCKED)
 
-    ok = c.command_gear_up()
+    ok = c.command_gear_up(True)
     assert ok is True
     assert c._state == GearState.TRANSITIONING_UP
     assert c.up_cmds[-1] is True
+
+
+def test_command_gear_up_false_without_active_retract_is_rejected_or_no_effect(config):
+    clock = FakeClock()
+    c = SpyLandingGearController(config=config, clock=clock)
+
+    c.enter_state(GearState.DOWN_LOCKED)
+
+    ok = c.command_gear_up(False)
+    assert ok in (True, False)
 
 
 @pytest.mark.parametrize(
@@ -114,118 +118,36 @@ def test_command_gear_up_happy_path_enters_transition_and_actuates(config):
         GearState.RESET,
     ],
 )
-def test_command_gear_up_rejected_if_not_down_locked(initial_state, config):
+def test_command_gear_up_true_rejected_if_not_down_locked(initial_state, config):
     clock = FakeClock()
     c = SpyLandingGearController(config=config, clock=clock)
 
     c.enter_state(initial_state)
 
-    ok = c.command_gear_up()
+    ok = c.command_gear_up(True)
     assert ok is False
     assert c._state == initial_state
     assert any("Retract rejected" in msg for msg in c.logs)
 
-
-def test_command_gear_up_idempotent_rejected_when_already_transitioning(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
-    assert c._state == GearState.TRANSITIONING_UP
-
-    ok2 = c.command_gear_up()
-    assert ok2 is False
-    assert c._state == GearState.TRANSITIONING_UP
-
-
-# -----------------------------
-# LGCS-FR002 timing behavior
-# -----------------------------
 
 def test_update_completes_retract_after_configured_time(config):
     clock = FakeClock()
     c = SpyLandingGearController(config=config, clock=clock)
 
     c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
+    assert c.command_gear_up(True) is True
     assert c._state == GearState.TRANSITIONING_UP
     assert c.up_cmds[-1] is True
 
     clock.advance(c._deploy_time_s - 1e-6)
     c.update()
     assert c._state == GearState.TRANSITIONING_UP
-    assert c.up_cmds[-1] is True
 
     clock.advance(1e-3)
     c.update()
     assert c._state == GearState.UP_LOCKED
     assert c.up_cmds[-1] is False
 
-
-def test_update_retract_edge_exactly_at_deploy_time(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
-    assert c._state == GearState.TRANSITIONING_UP
-
-    clock.advance(c._deploy_time_s)
-    c.update()
-
-    assert c._state == GearState.UP_LOCKED
-    assert c.up_cmds[-1] is False
-
-
-def test_update_retract_large_time_step_completes_in_one_tick(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
-    assert c._state == GearState.TRANSITIONING_UP
-
-    clock.advance(c._deploy_time_s * 10.0)
-    c.update()
-
-    assert c._state == GearState.UP_LOCKED
-    assert c.up_cmds[-1] is False
-
-
-def test_update_retract_time_goes_backward_does_not_complete(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
-    assert c._state == GearState.TRANSITIONING_UP
-
-    clock.advance(-1.0)
-    c.update()
-
-    assert c._state == GearState.TRANSITIONING_UP
-
-
-def test_update_during_transition_keeps_actuator_enabled_until_complete(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    assert c.command_gear_up() is True
-    assert c._state == GearState.TRANSITIONING_UP
-    assert c.up_cmds[-1] is True
-
-    clock.advance(c._deploy_time_s / 2.0)
-    c.update()
-
-    assert c._state == GearState.TRANSITIONING_UP
-    assert c.up_cmds[-1] is True
-
-
-# -----------------------------
-# LGCS-FR002 weight-on-wheels interlock behavior
-# -----------------------------
 
 @pytest.mark.skipif(
     not hasattr(LandingGearController, "set_weight_on_wheels"),
@@ -238,63 +160,20 @@ def test_fr002_blocks_retract_when_weight_on_wheels_true(config):
     c.enter_state(GearState.DOWN_LOCKED)
     c.set_weight_on_wheels(True)
 
-    ok = c.command_gear_up()
+    ok = c.command_gear_up(True)
     assert ok is False
     assert c._state == GearState.DOWN_LOCKED
     assert c.up_cmds == []
 
 
-@pytest.mark.skipif(
-    not hasattr(LandingGearController, "set_weight_on_wheels"),
-    reason="WoW gating not implemented yet",
-)
-def test_fr002_allows_retract_when_weight_on_wheels_false(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    c.set_weight_on_wheels(False)
-
-    ok = c.command_gear_up()
-    assert ok is True
-    assert c._state == GearState.TRANSITIONING_UP
-    assert c.up_cmds[-1] is True
-
-
-@pytest.mark.skipif(
-    not hasattr(LandingGearController, "set_weight_on_wheels"),
-    reason="WoW gating not implemented yet",
-)
-def test_fr002_wow_toggle_allows_retract_after_initial_block(config):
-    clock = FakeClock()
-    c = SpyLandingGearController(config=config, clock=clock)
-
-    c.enter_state(GearState.DOWN_LOCKED)
-    c.set_weight_on_wheels(True)
-
-    ok1 = c.command_gear_up()
-    assert ok1 is False
-    assert c._state == GearState.DOWN_LOCKED
-
-    c.set_weight_on_wheels(False)
-
-    ok2 = c.command_gear_up()
-    assert ok2 is True
-    assert c._state == GearState.TRANSITIONING_UP
-
-
-# -----------------------------
-# LGCS-FR004 retract ignored in FAULT / abnormal states
-# -----------------------------
-
 @pytest.mark.parametrize("abnormal_state", [GearState.FAULT, GearState.RESET])
-def test_retract_rejected_in_abnormal_states(abnormal_state, config):
+def test_fr004_retract_rejected_in_fault_or_abnormal_states(abnormal_state, config):
     clock = FakeClock()
     c = SpyLandingGearController(config=config, clock=clock)
 
     c.enter_state(abnormal_state)
 
-    ok = c.command_gear_up()
+    ok = c.command_gear_up(True)
     assert ok is False
     assert c._state == abnormal_state
     assert c.up_cmds == []
